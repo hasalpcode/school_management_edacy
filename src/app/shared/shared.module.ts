e2cfed10106f90225eaf5f7fe0d392268dfa867a
hasalp <hasalpcode784@gmail.com>
2022-05-30 08:26:26 +0000

    ajout module router pour la navigation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,11 +13,12 @@ import { FormsModule } from '@angular/forms';
     FooterComponent,
   ],
   exports:[
-    HeaderComponent,FooterComponent,FormsModule,
+    HeaderComponent,FooterComponent,FormsModule,RouterModule
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule,
   ]
 })
 export class SharedModule { }
